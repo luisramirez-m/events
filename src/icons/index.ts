5548589dc889352130a7234e98f458545968f9e5
Luis Ramirez <me.luisramirez@gmail.com>
2023-04-16 23:44:13 +0200

    feat(IconArrowBackIos,IconInfo,IconWarning): add icons

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,1 +1,4 @@
 export { default as IconCalendarToday } from './IconCalendarToday';
+export { default as IconArrowBackIos } from './IconArrowBackIos';
+export { default as IconInfo } from './IconInfo';
+export { default as IconWarning } from './IconWarning';
